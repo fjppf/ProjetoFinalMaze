910913ca5b8ac348558f0ca44bd7ed87752fd7e2
3426 Diogo Almeida <3426.diogo.almeida@eped.pt>
2024-06-09 15:54:26 +0100

    09/06/2024 - 16:00

--- src/controllers/cell_controller.py
+++ src/controllers/cell_controller.py
@@ -1,4 +1,5 @@
 from models.Cell import Cell
+from utils.log_exception import log_exception
 class CellController:
     # Constructor of the CellController class
     def __init__(self) -> None:
@@ -6,7 +7,11 @@ class CellController:
 
     # Method that allows the creation of a new cell
     def create_cell(self,x:int,y:int) -> Cell:
-        return Cell(x,y)
+        try:
+            return Cell(x, y)
+        except Exception as e:
+            log_exception(e)
+            return None  
     
     # Method that returns the value of the x attribute of the cell passed as a parameter
     def get_x(self,cell:Cell) -> int:
@@ -42,47 +47,56 @@ class CellController:
         
     # Method that find the neighbors if they exist around the cell
     def find_neighbors(self,check_cell_method:callable,cell:'Cell') -> list['Cell']:
-        # Check the top neighbor
-        top:'Cell' = check_cell_method(cell.get_x(), cell.get_y()-1)
-        # Check the right neighbor
-        right:'Cell' = check_cell_method(cell.get_x() + 1, cell.get_y())
-        # Check the bottom neighbor
-        bottom:'Cell' = check_cell_method(cell.get_x(), cell.get_y() + 1)
-        # Check the left neighbor
-        left:'Cell' = check_cell_method(cell.get_x() - 1, cell.get_y())
-        return (top,right,bottom,left)
+        try:
+            # Check the top neighbor
+            top:'Cell' = check_cell_method(cell.get_x(), cell.get_y()-1)
+            # Check the right neighbor
+            right:'Cell' = check_cell_method(cell.get_x() + 1, cell.get_y())
+            # Check the bottom neighbor
+            bottom:'Cell' = check_cell_method(cell.get_x(), cell.get_y() + 1)
+            # Check the left neighbor
+            left:'Cell' = check_cell_method(cell.get_x() - 1, cell.get_y())
+            return (top,right,bottom,left)
+        except Exception as e: # We mainly use this block because of the check_cell_method, as all the methods depend on it.
+            log_exception(e)
         
     # Method that sees which neighbors are available and chooses one to follow
     def check_neighbors_generate_maze(self,check_cell_method:callable,cell:'Cell') -> list['Cell']:
-        neighbors:list['Cell'] = []
-        # Neighbors
-        top,right,bottom,left = self.find_neighbors(check_cell_method,cell)
+        try:
+            neighbors:list['Cell'] = []
+            # Neighbors
+            top,right,bottom,left = self.find_neighbors(check_cell_method,cell)
 
-        # If there is a cell on top/right/bottom/left of the current cell, and that cell(top/right/bottom/left) it's not visited 
-        # gets added to the list.
-        if top and not top.get_visited():
-            neighbors.append(top)
-        if right and not right.get_visited():
-            neighbors.append(right)
-        if bottom and not bottom.get_visited(): 
-            neighbors.append(bottom)
-        if left and not left.get_visited(): 
-            neighbors.append(left)
-        return neighbors
+            # If there is a cell on top/right/bottom/left of the current cell, and that cell(top/right/bottom/left) it's not visited 
+            # gets added to the list.
+            if top and not top.get_visited():
+                neighbors.append(top)
+            if right and not right.get_visited():
+                neighbors.append(right)
+            if bottom and not bottom.get_visited(): 
+                neighbors.append(bottom)
+            if left and not left.get_visited(): 
+                neighbors.append(left)
+            return neighbors
+        except Exception as e:
+            log_exception(e)
     
     # Method that sees which neighbors are available and chooses one to follow
     def check_neighbors_algorithms(self,check_cell_method:callable,cell:'Cell') -> list['Cell']:
-        neighbors:list['Cell'] = []
-        top,right,bottom,left = self.find_neighbors(check_cell_method,cell) # Neighbors
-        # If there is a cell on top/right/bottom/left of the current cell, and that cell(top/right/bottom/left) it's not visited 
-        # gets added to the list.
-        if top and not top.get_visited() and not top.get_wall("bottom"):
-            neighbors.append(top)
-        if right and not right.get_visited() and not right.get_wall("left"):
-            neighbors.append(right)
-        if bottom and not bottom.get_visited() and not bottom.get_wall("top"): 
-            neighbors.append(bottom)
-        if left and not left.get_visited() and not left.get_wall("right"): 
-            neighbors.append(left)
-        return neighbors
+        try:
+            neighbors:list['Cell'] = []
+            top,right,bottom,left = self.find_neighbors(check_cell_method,cell) # Neighbors
+            # If there is a cell on top/right/bottom/left of the current cell, and that cell(top/right/bottom/left) it's not visited 
+            # gets added to the list.
+            if top and not top.get_visited() and not top.get_wall("bottom"):
+                neighbors.append(top)
+            if right and not right.get_visited() and not right.get_wall("left"):
+                neighbors.append(right)
+            if bottom and not bottom.get_visited() and not bottom.get_wall("top"): 
+                neighbors.append(bottom)
+            if left and not left.get_visited() and not left.get_wall("right"): 
+                neighbors.append(left)
+            return neighbors
+        except Exception as e:
+            log_exception(e)
     
